d534d4a05d3502704b882d0e520e208a3682bcdc
Bharath Parsam <bharathparsam@Bharaths-MacBook-Pro.local>
2020-09-16 15:49:38 +0530

    added service workers

--- src/app/portal/list/list.component.ts
+++ src/app/portal/list/list.component.ts
@@ -4,24 +4,18 @@ import { ConfigService } from 'src/app/services/config.service';
 @Component({
   selector: 'app-portal-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-
   @Input() queryFilter: string;
   @Input() data: any[];
 
-  constructor(private config: ConfigService) { }
+  constructor(private config: ConfigService) {}
 
   /**
    * Dumb Component (stateLess component)
    *
    * @memberof PortalComponent
    */
-  ngOnInit(): void {
-  }
-
-
-
-
+  ngOnInit(): void {}
 }
